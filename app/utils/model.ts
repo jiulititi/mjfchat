e292a79d49ad06d40c0e8809f836e342d1c568f2
MJF <jiulititi@gmail.com>
2024-06-23 09:29:35 +0800

    2.12.3

--- app/utils/model.ts
+++ app/utils/model.ts
@@ -1,25 +1,76 @@
 import { LLMModel } from "../client/api";
 
+const customProvider = (modelName: string) => ({
+  id: modelName,
+  providerName: "",
+  providerType: "custom",
+});
+
 export function collectModelTable(
   models: readonly LLMModel[],
   customModels: string,
 ) {
-  const modelTable: Record<string, boolean> = {};
+  const modelTable: Record<
+    string,
+    {
+      available: boolean;
+      name: string;
+      displayName: string;
+      provider?: LLMModel["provider"]; // Marked as optional
+      isDefault?: boolean;
+    }
+  > = {};
 
   // default models
-  models.forEach((m) => (modelTable[m.name] = m.available));
+  models.forEach((m) => {
+    modelTable[m.name] = {
+      ...m,
+      displayName: m.name, // 'provider' is copied over if it exists
+    };
+  });
 
   // server custom models
   customModels
     .split(",")
     .filter((v) => !!v && v.length > 0)
-    .map((m) => {
-      if (m.startsWith("+")) {
-        modelTable[m.slice(1)] = true;
-      } else if (m.startsWith("-")) {
-        modelTable[m.slice(1)] = false;
-      } else modelTable[m] = true;
+    .forEach((m) => {
+      const available = !m.startsWith("-");
+      const nameConfig =
+        m.startsWith("+") || m.startsWith("-") ? m.slice(1) : m;
+      const [name, displayName] = nameConfig.split("=");
+
+      // enable or disable all models
+      if (name === "all") {
+        Object.values(modelTable).forEach(
+          (model) => (model.available = available),
+        );
+      } else {
+        modelTable[name] = {
+          name,
+          displayName: displayName || name,
+          available,
+          provider: modelTable[name]?.provider ?? customProvider(name), // Use optional chaining
+        };
+      }
     });
+
+  return modelTable;
+}
+
+export function collectModelTableWithDefaultModel(
+  models: readonly LLMModel[],
+  customModels: string,
+  defaultModel: string,
+) {
+  let modelTable = collectModelTable(models, customModels);
+  if (defaultModel && defaultModel !== "") {
+    modelTable[defaultModel] = {
+      ...modelTable[defaultModel],
+      name: defaultModel,
+      available: true,
+      isDefault: true,
+    };
+  }
   return modelTable;
 }
 
@@ -31,10 +82,21 @@ export function collectModels(
   customModels: string,
 ) {
   const modelTable = collectModelTable(models, customModels);
-  const allModels = Object.keys(modelTable).map((m) => ({
-    name: m,
-    available: modelTable[m],
-  }));
+  const allModels = Object.values(modelTable);
 
+  return allModels;
+}
+
+export function collectModelsWithDefaultModel(
+  models: readonly LLMModel[],
+  customModels: string,
+  defaultModel: string,
+) {
+  const modelTable = collectModelTableWithDefaultModel(
+    models,
+    customModels,
+    defaultModel,
+  );
+  const allModels = Object.values(modelTable);
   return allModels;
 }
